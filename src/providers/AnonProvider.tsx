4e73f64a8130aa744eb752ae6c4dcb66fea7da94
Aman Tiwari <amananjalitiwari2007@gmail.com>
2024-07-12 02:03:21 +0530

    fix for vercel

--- src/providers/AnonProvider.tsx
+++ src/providers/AnonProvider.tsx
@@ -4,12 +4,6 @@ interface AnonProviderProps {
 	children: React.ReactNode;
 }
 
-const links = {
-	zkey_url: 'http://localhost:5173/circuit_final.zkey',
-	wasm_url: 'http://localhost:5173/aadhaar-verifier.wasm',
-	vkey_url: 'http://localhost:5173/vkey.json',
-};
-
 export default function AnonProvider({ children }: AnonProviderProps) {
 	return <AnonAadhaarProvider>{children}</AnonAadhaarProvider>;
 }
